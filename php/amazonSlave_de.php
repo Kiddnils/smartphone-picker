4664856d3b31e324c9318eeb0538f333d78caa3e
Martin Schulze Beckendorf <matt.sb@hotmail.de>
2018-05-18 09:51:08 +0200

    Formatting

--- php/amazonSlave_de.php
+++ php/amazonSlave_de.php
@@ -3,144 +3,82 @@
 $fileKeys = "../scripts/keys.js";
 $jsonKeys = json_decode(file_get_contents($fileKeys),TRUE);
 
-
 // Your Access Key ID, as taken from the Your Account page
-
 $access_key_id = $jsonKeys['access_key_id'];
 
-
-
 // Your Secret Key corresponding to the above ID, as taken from the Your Account page
-
 $secret_key = $jsonKeys['secret_key'];;
 
-
-
-// The region you are interested in
-
+// Amazon marketplace/region
 $endpoint = "webservices.amazon.de";
 
-
-
 $uri = "/onca/xml";
-
-
-
-
-
-
-
-
-
 
 $file = "../scripts/smartphones.js";
 $json = json_decode(file_get_contents($file),TRUE);
 
-
 for ($x = 0; $x <= count($json['smartphones']); $x++) {
     echo "The number is: $x <br>";
 
-
     $params = array(
-
     "Service" => "AWSECommerceService",
-
     "Operation" => "ItemLookup",
-
     "AWSAccessKeyId" => $access_key_id,
-
     "AssociateTag" => "smartphonep08-21",
-
     "ItemId" => "B07CHQHFDZ",
-
     "IdType" => "ASIN",
-
     "ResponseGroup" => "Medium"
-
     );
 
-if ($json['smartphones'][$x]['asin_de'] != '') {
-    $params["ItemId"] = $json['smartphones'][$x]['asin_de'];
-    echo $params["ItemId"];
+  if ($json['smartphones'][$x]['asin_de'] != '') {
+      $params["ItemId"] = $json['smartphones'][$x]['asin_de'];
+      echo $params["ItemId"];
 
+      // Set current timestamp if not set
+      if (!isset($params["Timestamp"])) {
+        $params["Timestamp"] = gmdate('Y-m-d\TH:i:s\Z');
+      }
 
+      // Sort the parameters by key
+      ksort($params);
 
-    // Set current timestamp if not set
+      $pairs = array();
+      foreach ($params as $key => $value) {
+        array_push($pairs, rawurlencode($key)."=".rawurlencode($value));
+      }
 
-    if (!isset($params["Timestamp"])) {
+      // Generate the canonical query
+      $canonical_query_string = join("&", $pairs);
 
-    $params["Timestamp"] = gmdate('Y-m-d\TH:i:s\Z');
+      // Generate the string to be signed
+      $string_to_sign = "GET\n".$endpoint."\n".$uri."\n".$canonical_query_string;
 
+      // Generate the signature required by the Product Advertising API
+      $signature = base64_encode(hash_hmac("sha256", $string_to_sign, $secret_key, true));
+
+      // Generate the signed URL
+      $request_url = 'http://'.$endpoint.$uri.'?'.$canonical_query_string.'&Signature='.rawurlencode($signature);
+
+  echo "Signed URL: \"".$request_url."\"";
+
+  $response = file_get_contents($request_url);
+
+  if($response === FALSE)
+  {
+    echo "Amazon blocked";
+  }else {
+      $xml = new DOMDocument();
+      $xml->loadXML($response);
+      $items = $xml->getElementsByTagName("Items")[0];
+      foreach ( $items->getElementsByTagName("Item") as $item )    {
+        $json['smartphones'][$x]['amazon_de'] = $item->getElementsByTagName("DetailPageURL")[0]->nodeValue.PHP_EOL;
+        $json['smartphones'][$x]['price_de'] = (int)substr($item->getElementsByTagName("Amount")[0]->nodeValue.PHP_EOL, 0, -3);
+      }
+      sleep(4);
     }
-
-
-
-    // Sort the parameters by key
-
-    ksort($params);
-
-
-
-    $pairs = array();
-
-
-
-    foreach ($params as $key => $value) {
-
-    array_push($pairs, rawurlencode($key)."=".rawurlencode($value));
-
-    }
-
-
-
-    // Generate the canonical query
-
-    $canonical_query_string = join("&", $pairs);
-
-
-
-    // Generate the string to be signed
-
-    $string_to_sign = "GET\n".$endpoint."\n".$uri."\n".$canonical_query_string;
-
-
-
-    // Generate the signature required by the Product Advertising API
-
-    $signature = base64_encode(hash_hmac("sha256", $string_to_sign, $secret_key, true));
-
-
-
-    // Generate the signed URL
-
-    $request_url = 'http://'.$endpoint.$uri.'?'.$canonical_query_string.'&Signature='.rawurlencode($signature);
-
-echo "Signed URL: \"".$request_url."\"";
-
-//echo "Signed URL: \"".$response."\"";
-
-$response = file_get_contents($request_url);
-
-if($response === FALSE)
-{
-  echo "Amazon blocked";
-}else {
-    $xml = new DOMDocument();
-    $xml->loadXML($response);
-    $items = $xml->getElementsByTagName("Items")[0];
-    foreach ( $items->getElementsByTagName("Item") as $item )    {
-    $json['smartphones'][$x]['amazon_de'] = $item->getElementsByTagName("DetailPageURL")[0]->nodeValue.PHP_EOL;
-
-    $json['smartphones'][$x]['price_de'] = (int)substr($item->getElementsByTagName("Amount")[0]->nodeValue.PHP_EOL, 0, -3);
-    }
-    sleep(4);
-
   }
 }
-}
+
 file_put_contents("../scripts/smartphones.js", json_encode($json));
 echo "Files should be updated";
-
-
-
 ?>
